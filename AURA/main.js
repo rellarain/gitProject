0bd927a3a21d5266b224b4a9568a86f862c8fd25
Rella <aurella@auraprogram.com>
2019-06-17 15:14:08 -0400

    afternoon 6/17

---
+++ AURA/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+var name = "My name is Rain";
+var hobby = "I like to study psychology and sociology.";
+var years = 4.5;
+
+console.log(name, "and", hobby, "I was in school for", years, "years.");
