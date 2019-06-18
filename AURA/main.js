ce6f2ee28c564b3e216644aa90686424e15f8edc
Rella <aurella@auraprogram.com>
2019-06-18 16:01:27 -0400

    6/18 AURA

--- AURA/main.js
+++ AURA/main.js
@@ -1,7 +1,15 @@
 "use strict"
 
 var x = 0;
-while (x <= 100) {console.log(x++);
-    if (x%3 === 0 && x%5 === 0) {console.log(x++ + " fizzbuzz")};
-    if (x%3 === 0) {console.log(x++ + " fizz")};
-    if (x%5 === 0) {console.log(x++ + " buzz")};};
+while (x <= 100) {
+    if (x%3 === 0 && x%5 === 0) {
+        console.log(x++, "fizzbuzz");
+    }
+    else if (x%3 === 0) {
+        console.log(x++, "fizz");
+    }
+    else if (x%5 === 0) {
+        console.log(x++, "buzz");
+    }
+    else {console.log(x++);}
+};
