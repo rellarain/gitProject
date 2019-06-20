e0f0818cf7ad98c1bda66c47c9ec04504ae843a4
Rella <aurella@auraprogram.com>
2019-06-20 15:07:09 -0400

    6/20

--- AURA/main.js
+++ AURA/main.js
@@ -1,15 +1,50 @@
 "use strict"
 
-var x = 0;
-while (x <= 100) {
-    if (x%3 === 0 && x%5 === 0) {
-        console.log(x++, "fizzbuzz");
-    }
-    else if (x%3 === 0) {
-        console.log(x++, "fizz");
-    }
-    else if (x%5 === 0) {
-        console.log(x++, "buzz");
-    }
-    else {console.log(x++);}
+function add(a,b) {
+    var answer = a + b;
+    console.log(answer);
 };
+add(6,7);
+//------------------------------------------------------------
+function multiply(c,d) {
+    var answer = c * d;
+    console.log(answer);
+}
+
+multiply(5,6);
+//--------------------------------------------------------------------
+function lister(e,f) {
+    for (e=e+1; e<f; ++e) {
+        console.log(e);
+    };
+}
+
+lister(20,40)
+//-----------------------------------------------------------------------
+var g = 5;
+
+function divider(h) {
+    var answer = (g*h)/h;
+    console.log(answer);
+}
+
+divider (6);
+
+//--------------------------------------------------------------
+//Matcher Challenge
+
+
+function matcher() {
+    var user = prompt();
+    var getRandom = Math.floor(Math.random()*10);
+        if (user == getRandom) {
+            console.log("Success")
+        }
+        else {
+            console.log("Failure")
+        };
+}
+
+matcher();
+
+////--------------------------------------------
