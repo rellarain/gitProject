e5f0c64297d2c08ecffe6a7d9e06427f1b4753c0
rain <aurella@auraprogram.com>
2019-07-09 14:27:04 -0400

    7/9 x2

--- cbc6-lessons-week-3-quiz/js/nodeExercise/nodePrompt.js
+++ cbc6-lessons-week-3-quiz/js/nodeExercise/nodePrompt.js
@@ -5,25 +5,21 @@ var rl = readline.createInterface({
 });
 
  rl.question("What is your sun sign?\n", function(answer){
-   rl.close();
    finished1(answer);
-});
-rl.question("What is your moon sign?\n", function(answer){
-   rl.close();
-   finished2(answer);
-});
-rl.question("What is your rising sign?\n", function(answer){
-   rl.close();
-   finished3(answer);
-});
-
-
-function finished1(value) {
-   console.log("Okay, so you are a " + value +" sun sign.")
-};
-function finished2(value) {
-   console.log("Okay, so you are a " + value +" moon sign.")
-};
-function finished3(value) {
-   console.log("Okay, so you are a " + value +" rising sign.")
+   rl.question("What is your moon sign?\n", function(answer){
+      finished2(answer);
+      rl.question("What is your rising sign?\n", function(answer){
+         finished3(answer);
+         rl.close();
+      });
+   });
+ });
+      function finished1(value) {
+         console.log("Okay, so you are a " + value +" sun sign.")
+      };
+      function finished2(value) {
+         console.log("Okay, so you are a " + value +" moon sign.")
+      };
+      function finished3(value) {
+         console.log("Okay, so you are a " + value +" rising sign.")
 };
